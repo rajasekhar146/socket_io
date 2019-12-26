f108f905efb37c9d0341a15b1a13468cd63f3fd6
rajasekhar <rajasekharmtiet@gmail.com>
2019-12-26 16:14:12 +0530

    second  message

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
-var app = express();
+let app = express();
 
+let server = http.createServer(app)
+let io = socketIO(server)
 app.use(express.static(publicPath))
-app.listen(port, () => {
+
+io.on('connection', (socket)=> {
+    console.log("A new user connected")
+    socket.on('disconnect', () => {
+        console.log("dosconneted from server")
+    })
+})
+
+server.listen(port, () => {
     console.log(`server lostening on ${port}`)
 })
